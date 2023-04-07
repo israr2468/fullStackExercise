82fd398df091c35af8e91ecc05020dd886b7db74
Israr Ali <israrali2468@gmail.com>
2023-04-07 10:04:59 -0700

    modified:   .env 	modified:   public/app.js 	modified:   server.js

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 console.log('Fetching user data...')
 
 const userContainer = document.getElementsByClassName('user-container')[0];
-fetch('http://localhost:3001/api/mydata')
+fetch('/api/mydata')
     .then(response => response.json())
     .then(data => {
         data.forEach(user => {
             const userElement = document.createElement('div');
-            userElement.textContent = `Name: ${user.name} Number: ${user.number} Email: ${user.email}`;
+            userElement.textContent = `Name: ${user.name} Number: ${user.number} Email:${user.email}`;
             userContainer.appendChild(userElement);
         });
     })
--- server.js
+++ server.js
@@ -7,35 +7,50 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5501'
+    //origin: 'http://127.0.0.1:5501'
 }));
 
 dotenv.config();
 
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
-// const pool = new Pool ({connectionString: DATABASE_URL});
-
-pool.connect();
-
-app.get('/api/mydata', (req, res) => {
-    pool.query('SELECT * FROM mytable', (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error fetching data');
-      } else {
-        res.json(result.rows);
-      }
-    });
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
+  
+
+async function connect() {
+  try {
+    await pool.connect();
+    console.log('Database connection successful');
+  } catch (err) {
+    console.error('Error connecting to the database', err);
+  }
+}
+
+connect();
+
+app.get('/api/mydata', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM mytable');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching data', err);
+    res.status(500).send('Error fetching data');
+  }
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
 });
 
-console.log('this is server')
+console.log('this is server');
